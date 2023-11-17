10b76b41165138aefe54f464564612e80574ee03
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2023-11-17 17:52:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -108,7 +108,7 @@ loadChatgptDB();
 
 /* ------------------------------------------------*/
 
-global.authFile = `CherBot`;
+global.authFile = `MichiBot`;
 const {state, saveState, saveCreds} = await useMultiFileAuthState(global.authFile);
 const msgRetryCounterMap = (MessageRetryMap) => { };
 const msgRetryCounterCache = new NodeCache()
